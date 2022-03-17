1a99d8b039ef6963a64f7a05828902d92a00c9fd
Sunny Yadav <sunny.y@simformsolutions.com>
2022-03-17 19:43:02 +0530

    Changes to code. Implemented registration and html template

--- DevWorld/DevWorld/settings.py
+++ DevWorld/DevWorld/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'DevWorld',
         'USER': 'postgres',
-        'PASSWORD': '12345',
+        'PASSWORD': 'Simform@123',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
